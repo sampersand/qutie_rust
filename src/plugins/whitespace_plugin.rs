1e258cfd66b3fe1a603167fe929130211d6aac82
westerhack <westerhack@gmail.com>
2017-03-14 16:55:08 -0700

    Whitespace works

--- src/plugins/whitespace_plugin.rs
+++ src/plugins/whitespace_plugin.rs
@@ -1,27 +1,24 @@
 use plugins::plugin::Plugin;
 use environment::Environment;
 use plugins::plugin::PluginResponse;
-use plugins::plugin::PluginResponse::{NoResponse, Retry};
-use objects::universe::Universe;
 use objects::boxed_obj::BoxedObj;
-use objects::single_character::SingleCharacter;
-use objects::object::ObjectType;
 
 #[derive(Debug)]
-pub struct WhitespacePlugin{}
+pub struct WhitespacePlugin;
 
 pub static INSTANCE: WhitespacePlugin = WhitespacePlugin{};
 
 impl Plugin for WhitespacePlugin {
    fn next_object(&self, env: &mut Environment) -> PluginResponse {
-      if match env.stream.peek_char() {
+      let is_whitespace = match env.stream.peek_char() {
          Some(obj) => obj.source_val.is_whitespace(),
          None => false
-      } {
+      };
+      if is_whitespace {
          env.stream.next();
-         Retry
+         PluginResponse::Retry
       } else {
-         NoResponse
+         PluginResponse::NoResponse
       }
    }
    fn handle(&self, token: BoxedObj, env: &mut Environment) {}
